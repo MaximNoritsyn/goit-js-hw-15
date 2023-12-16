900e1c182cc33de57a9b0dd7b3f629b1dde3fa1b
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-16 13:50:10 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+//elements
+const loginFormEl = document.querySelector('.login-form');
 
+//functions
+function loginFormFunc(event) {
+    event.preventDefault();
+    const elTarget = event.target;
+    const {email, password} = elTarget.elements;
+    if (email.value === '' || password.value === '') {
+        let message = '';
+        if (email.value === '') {
+            message += 'email ';
+        }
+        if (password.value === '') {
+            message += 'password ';
+        }
+        alert(`Please, enter ${message}`);
+    } else {
+        const user = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(user);
+        elTarget.reset();
+    }
+}
+
+//events
+loginFormEl.addEventListener('submit', loginFormFunc);
