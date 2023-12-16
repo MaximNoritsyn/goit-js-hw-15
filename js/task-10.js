64ba71b8b838ee944de3ba193c9c238e59678706
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-16 15:06:53 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,37 @@
+// elements
+const buttonStart = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const amountEl = document.querySelector('#controls input');
+const boxesEl = document.querySelector('#boxes');
+
+// functions
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  boxesEl.append(...boxes);
+}
+
+function handlerCreateBoxes() {
+  createBoxes(amountEl.value);
+}
+
+function handlerDestroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+// events
+buttonStart.addEventListener('click', handlerCreateBoxes);
+buttonDestroy.addEventListener('click', handlerDestroyBoxes);
