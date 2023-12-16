21e36160aa6e431633d32ca37593371e09317412
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-16 14:28:24 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,20 @@
+// elements
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+// functions
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
+
+// events
+buttonEl.addEventListener('click', changeColor);
