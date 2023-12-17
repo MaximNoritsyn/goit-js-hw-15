7df658a87c01b01cb93e510b07a76d799e704d82
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-17 17:05:11 +0200

    fix task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,16 @@
 // elements
 const fontSizeControl = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+const fotnSizeEl = document.querySelector('#font-size-control');
+
 
 //functions
-function fontSizeControlFunc(event) {
-    text.style.fontSize = event.target.value + 'px';
+function fontSizeControlFunc() {
+    text.style.fontSize = fotnSizeEl.value + 'px';
 }
+
+//call functions
+fontSizeControlFunc();
 
 //events
 fontSizeControl.addEventListener('input', fontSizeControlFunc);
